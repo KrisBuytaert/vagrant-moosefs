a9a3bd858ac258ac949cefffdd172d504e94fa70
Kris Buytaert <sdog@inuits.be>
2012-01-06 21:54:51 +0100

    Layout updates + Vagrant includes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,75 +1,87 @@
 class repo {
 
-	# Enable RPMForge 
+  # Enable RPMForge 
 
 
-yumrepo { 
-
-  "rpmforge":
-                                descr => "RPMforge.net - dag",
-                                baseurl => absent,
-                                mirrorlist => "http://apt.sw.be/redhat/el5/en/mirrors-rpmforge",
-                                enabled => "1",
-                                gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-dag",
-                                gpgcheck => "0",
-                                priority => 1; 
-	}
+  yumrepo {
+    "rpmforge":
+      descr => "RPMforge.net - dag",
+      baseurl => absent,
+      mirrorlist => "http://apt.sw.be/redhat/el5/en/mirrors-rpmforge",
+      enabled => "1",
+      gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-dag",
+      gpgcheck => "0",
+      priority => 1; 
+  }
 
 }
 
 
 class mine {
 
-
-	package {
-	"iptables": ensure => absent;
-	"system-config-securitylevel-tui": ensure => absent;
-	"iptables-ipv6": ensure => absent;
-	"iptstate": ensure => absent;
-	}
-
-
-#	service { "iptables":
-#                ensure => "stopped",
-#                enable => "false";
-#                }
+  package {
+    "iptables": ensure => absent;
+    "system-config-securitylevel-tui": ensure => absent;
+    "iptables-ipv6": ensure => absent;
+    "iptstate": ensure => absent;
+  }
 
 
-	include repo
+  #	service { "iptables":
+  #                ensure => "stopped",
+  #                enable => "false";
+  #                }
 
+
+  include repo
 
 }
 
 
 
-node moose1{ 
-	include mine
-	include moosefs::server
-	$mfsmaster = "192.168.99.101"
-	include moosefs::mfsmaster
-	file {"/mnt/storage": ensure=> directory, mode => 777; }
-	$mountpoints = "/mnt/storage"
-	include moosefs::chunk
+node moose1{
+
+  include mine
+  include moosefs::server
+  $mfsmaster = "192.168.99.101"
+  include moosefs::mfsmaster
+  file {
+     "/mnt/storage":
+      ensure=> directory,
+      mode => 777;
+  }
+  $mountpoints = "/mnt/storage"
+  include moosefs::chunk
 }
-node moose2{ 
-	include mine
-	include moosefs::server
-	$mfsmaster = "192.168.99.101"
-	include moosefs::metalogger
-	file {"/mnt/storage": ensure=> directory, mode => 777; }
-	$mountpoints = "/mnt/storage"
-	include moosefs::chunk
+
+node moose2{
+  include mine
+  include moosefs::server
+  $mfsmaster = "192.168.99.101"
+  include moosefs::metalogger
+  file {
+    "/mnt/storage":
+      ensure => directory,
+      mode   => 777;
+  }
+  $mountpoints = "/mnt/storage"
+  include moosefs::chunk
 }
-node moose3{ 
-	include mine
-  	include moosefs::client 
-	file {"/mnt/moose/": ensure => directory }
-	$mfsmaster = "192.168.99.101"
-	mount { "/mnt/moose":
-        		atboot => true,
-        		device => "mfsmount",
-        		ensure => mounted,
-        		fstype => "fuse",
-        		options => "mfsmaster=$mfsmaster,mfsport=9421,_netdev"; }
+
+node moose3{
+  include mine
+  include moosefs::client 
+  file {
+    "/mnt/moose/": 
+    ensure => directory 
+  }
+  $mfsmaster = "192.168.99.101"
+  mount {
+    "/mnt/moose":
+      atboot   => true,
+      device   => "mfsmount",
+      ensure   => mounted,
+      fstype   => "fuse",
+      options  => "mfsmaster=$mfsmaster,mfsport=9421,_netdev"; }
 
 }
